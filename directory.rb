04d560ac86515387655e7d043bafee99ca1af74f
Brooke Woolley <brooke.woolley@gmail.com>
2018-12-17 22:09:08 +0000

    Chap8 Exercise 9: fixed footer

--- directory.rb
+++ directory.rb
@@ -1,19 +1,3 @@
-# let's put all the students into an array
-# students = [
-#   {name: "Dr. Hannibal Lecter", cohort: :november},
-#   {name: "Darth Vader", cohort: :november},
-#   {name: "Nurse Ratched", cohort: :november},
-#   {name: "Michael Corleone", cohort: :november},
-#   {name: "Alex DeLarge", cohort: :november},
-#   {name: "The Wicked Witch of the West", cohort: :november},
-#   {name: "Terminator", cohort: :november},
-#   {name: "Freddy Kruger", cohort: :november},
-#   {name: "The Joker", cohort: :november},
-#   {name: "Joffrey Baratheon", cohort: :november},
-#   {name: "Norman Bates", cohort: :november}
-# ]
-
-
 def input_students
   puts "Please enter the name of the student"
   puts "To finish, just hit return twice"
@@ -22,16 +6,20 @@ def input_students
   name = gets.chomp
   # while the name if not empty, repeat this code
   while !name.empty? do
-    # add the student hash to the array
+    puts "What cohort are you joining?"
+    cohort = gets.chomp
+    cohort_list = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
     puts "Please enter your birth country."
     country = gets.chomp
     puts "Please enter your height in cm."
     height = gets.chomp
-        students << {name: name, cohort: :november, country: country, height: height}
+    if cohort_list.include?(cohort)
+      students << {name: name, cohort: cohort.downcase.to_sym, country: country, height: height}
+    end
     if students.length > 1
       puts "Now we have #{students.count} students."
     else
-      puts "now we have 1 student."
+      puts "Now we have 1 student."
     end
     # get another name from the user
     name = gets.chomp
@@ -48,7 +36,7 @@ end
 def print(students)
   students_index = 0
   while students.length > students_index do
-    puts "#{students[students_index][:name]} (#{students[students_index][:cohort]} cohort) is from #{students[students_index][:country]} and is #{students[students_index][:height]}cm.".center(60)
+    puts "#{students[students_index][:name]} (#{students[students_index][:cohort].capitalize} cohort) is from #{students[students_index][:country]} and is #{students[students_index][:height]}cm.".center(60)
     students_index += 1
   end
 end
@@ -73,7 +61,11 @@ end
 # end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  if students.length > 1
+    puts "Overall, we have #{students.count} great students"
+  else
+    puts "We have 1 great student"
+  end
 end
 
 
