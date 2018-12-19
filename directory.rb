2e4bfd57bdb8c98517a7a119741cd033ca644e0e
Brooke Woolley <brooke.woolley@gmail.com>
2018-12-19 22:18:27 +0000

    Chap 9: interactive menu

--- directory.rb
+++ directory.rb
@@ -64,13 +64,37 @@ def print_footer(students)
   end
 end
 
+def interactive_menu
+  students = []
+  loop do
+  puts "1. Input the students."
+  puts "2. Show the students."
+  puts "9. Exit."
+  selection = gets.chomp
 
-students = input_students
-print_header
-print(students)
-# print_selected_letter(students)
-# print_12_character(students)
-print_footer(students)
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    print_header
+    print(students)
+    print_footer(students)
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again."
+  end
+end
+end
+
+interactive_menu
+
+# students = input_students
+# print_header
+# print(students)
+# # print_selected_letter(students)
+# # print_12_character(students)
+# print_footer(students)
 
 # def print_selected_letter(students)
 #   puts "What names would you like me to return by first-letter?"
