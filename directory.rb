744caf641409447d6cee9b3142f3b4b2aefe6892
Brooke Woolley <brooke.woolley@gmail.com>
2018-12-22 15:15:06 +0000

    Chap 14, Exercise 1: creating new method to simplify code

--- directory.rb
+++ directory.rb
@@ -7,8 +7,7 @@ def input_students
   name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
-    # add the student hash to the array
-    @students << {name: name, cohort: :november}
+    add_students(name)
     puts "Now we have #{@students.count} students"
     # get another name from the user
     name = STDIN.gets.chomp
@@ -50,9 +49,13 @@ def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
+    add_students(name, cohort.to_sym)
   end
   file.close
+end
+
+def add_students(name, cohort = :november)
+  @students << {name: name, cohort: cohort.to_sym}
 end
 
 def try_load_students
